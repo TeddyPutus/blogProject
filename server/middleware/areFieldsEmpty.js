e3f88868ff5c73e8bbc1693faf67b574c8c71bfa
lysirebe <lysirebe@gmail.com>
2022-11-25 10:26:06 +0000

    validation on Post and Put Requests work

---
+++ server/middleware/areFieldsEmpty.js
@@ -0,0 +1,13 @@
+// const {body} = require('express-validator');
+
+// function areFieldsEmpty(next)  {
+
+//   const fields = ['author','content', 'category','title']
+//   for(const field in fields){
+//       body(field).not().isEmpty().withMessage(`${field} field is Empty`)
+//   }
+//   next();
+
+// }
+
+// module.exports= areFieldsEmpty
